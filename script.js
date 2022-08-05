0ae78283c50ed8551d3f057591b1a682b99e4c91
Diogo Schoninger <schoninger.diogo@gmail.com>
2022-08-05 11:28:43 -0300

    Display adjustment

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ btnAdd.onclick = () => {
   productIndex++;
 
   productsData.insertAdjacentHTML('beforeend', `
-    <div id="productBlock${productIndex}">
+    <div id="productBlock${productIndex}" class="inputGroup">
       <input type="text" placeholder="Descrição do produto" id="productDescription${productIndex}"/>
       <input type="number" placeholder="Preço" id="productPrice${productIndex}"/>
     </div>
@@ -26,9 +26,9 @@ btnPrint.onclick = () => {
     price = price.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL', minimumFractionDigits: 2});
 
     printable.insertAdjacentHTML('beforeend', `
-      <div style="height: 30.639cm; width: 21cm; display: block;">
-        <div style="display: flex; align-items: center; height: 50%; text-align: center; font-size: 2.5cm; font-weight: bold; font-family: Arial, sans-serif">${description}</div>
-        <div style="margin: 0; display: flex; align-items: center; text-align: center; height: 50%; text-align: center; font-size: 5cm; font-weight: bold; font-family: Arial, sans-serif">${price}</div>
+      <div style="height: 30.639cm; width: 21cm; display: block">
+        <div style="margin: 0; width: 100%; display: flex; align-items: center; text-align: center; height: 50%; font-size: 2.5cm; font-weight: bold; font-family: Arial, sans-serif">${description}</div>
+        <div style="margin: 0; width: 100%; display: flex; align-items: center; justify-content: center; height: 50%; font-size: 5cm; font-weight: bold; font-family: Arial, sans-serif">${price}</div>
       </div>
     `);
   }
