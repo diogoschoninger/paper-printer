9531dcf87062fecc7c250a85bdecf2f726dd0bcd
Diogo Schoninger <schoninger.diogo@gmail.com>
2022-10-24 15:42:38 -0300

    A4 sheet working

--- public/js/ofertas.js
+++ public/js/ofertas.js
@@ -70,6 +70,27 @@ const priceInputFilter = (element) => {
   element.value = formattedPrice
 }
 
+const print = (products, prices) => {
+  printable = document.getElementById('printable')
+
+  printable.innerHTML = ''
+
+  for (let i = 0; i < products.length; i++) {
+    printable.insertAdjacentHTML('beforeend', `
+      <div style="height: 29.7cm; width: 21cm; padding-top: 3rem">
+        <div style="text-align: center; font-weight: bold; color: rgba(0, 0, 0, .4); font-size: 7rem; height: 15%">OFERTA</div>
+
+        <div style="height: 85%; display: flex; flex-direction: column; justify-content: space-evenly; align-items: center">
+          <div style="color: #000; font-size: 6rem; font-weight: bold; text-align: center">${products[i].toUpperCase()}</div>
+          <div style="color: #000; font-size: 12rem; font-weight: bold; text-align: center">R$ ${prices[i]}</div>
+        </div>
+      </div>
+    `)
+  }
+
+  window.print()
+}
+
 const submitForm = (event) => {
   event.preventDefault()
 
@@ -87,6 +108,8 @@ const submitForm = (event) => {
       prices.push(priceInput.value)
     }
   }
+
+  print(products, prices)
 }
 
 addProductField()
